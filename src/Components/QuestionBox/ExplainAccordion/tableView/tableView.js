9354ddd46ba2d27af82eb3e33a60032797e3966e
Marli Renee <marlirenee@gmail.com>
2020-09-04 15:02:06 -0700

    changes to tableView

--- src/Components/QuestionBox/ExplainAccordion/tableView/tableView.js
+++ src/Components/QuestionBox/ExplainAccordion/tableView/tableView.js
@@ -3,21 +3,28 @@ import React, {Component} from 'react'
 import './tableView.css'
 
 export default class TableView extends Component {
-
+    
+    // constructor(props) {
+    //     super(props);
+    //     this.state = {
+    //       data: this.props.vocabArray,
+    //     };
+    // }
 
     render() {
         
-        console.log(this.props)
+        console.log(this.props.data);
+        const data = this.props.data;
 
     
         return (
             <div classname="tableView">
 
             <div className="row">
-
+                    
                     <div className="column">
                         <ul>
-                            <li><p>{this.props.data}</p></li>
+                            {this.props.data.map(d => <li>{d.vocabArray}</li>)}
                         </ul>
                     </div>
                     <div className="column">
@@ -37,7 +44,18 @@ export default class TableView extends Component {
         )
     }
 
-
 } 
 
+
+// {this.props.data.length > 0 && 
+//     this.props.data.map(
+//         ({vocabArray}) => (
+//             <div className="column">
+//                         <ul>
+//                             <li><p>{this.props.data}</p></li>
+//                         </ul>
+//             </div>
+//         )
+//     )
+// }
   
