1269d911e411213065f5188be20fd9797253694e
Marli Renee <marlirenee@gmail.com>
2020-09-04 12:06:51 -0700

    adding ExplainAccordion

--- src/Components/QuestionBox/QuestionBox.js
+++ src/Components/QuestionBox/QuestionBox.js
@@ -41,7 +41,6 @@ export default class QuestionBox extends Component {
                                 <ExplainAccordion 
                                     vocabTitle={vocabTitle} 
                                     vocabArray={vocabArray}
-
                                 />
                             </div>
 
