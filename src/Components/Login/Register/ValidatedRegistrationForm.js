a6c32a5a2588bd89dea68cd94401b8e86c618e21
Marli Renee <marlirenee@gmail.com>
2020-09-15 15:05:01 -0700

    implementing user registration

---
+++ src/Components/Login/Register/ValidatedRegistrationForm.js
@@ -0,0 +1,57 @@
+import React, { useState } from 'react'
+import config from '../../../config'
+import { Button } from '../../Utils/Utils'
+
+import './LoginRegister.css'
+
+export default function ValidatedRegistrationForm () {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  function validateForm() {
+    return username.length > 0 && password.length > 0;
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    const user = {
+        username: username,
+        password: password,
+    }
+
+    fetch(`${config.API_ENDPOINT}/users`, {
+      method: 'POST',
+      body: JSON.stringify(user),
+      headers: {
+          'content-type': 'application/json',
+      }
+    })
+    console.log(user)
+  }
+
+  return (
+    <div className="Login">
+      <form onSubmit={handleSubmit}>
+          <h3>Username</h3>
+          <input
+            autoFocus
+            type="text"
+            value={username}
+            onChange={e => setUsername(e.target.value)}
+          />
+          <h3>Password</h3>
+          <input
+            value={password}
+            onChange={e => setPassword(e.target.value)}
+            type="password"
+          />
+      </form>
+      <Button disabled={!validateForm()} type="submit">
+          Sign Up
+      </Button>
+
+    </div>
+  );
+
+}
